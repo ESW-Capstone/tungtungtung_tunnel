ca962f515b63574eb7602f8a97e090f7104537ba
나현준 <63279017+nhj1472@users.noreply.github.com>
2025-07-29 19:06:32 +0900

    Update nozzle_move.py

--- motor/nozzle_move.py
+++ motor/nozzle_move.py
@@ -3,20 +3,20 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-ser = serial.Serial('COM6', 9600)
+ser = serial.Serial('COM11', 9600)
 time.sleep(2)
 
-CM_PER_STEP = 1 / 450
+CM_PER_STEP = 1 / 450   #cm당 스텝수 여기를 조정하면 된다!
 DEG_PER_STEP = 360 / 2048
 MIN_DELAY_MS = 2.0
 
-def send_command(motor_id, direction, speed, distance):
+def send_command(motor_id, direction, speed, distance):     #모터id, 방향, 속도, 거리를 출력/송신
     cmd = f"{motor_id},{direction},{speed:.2f},{distance:.2f}\n"
     ser.write(cmd.encode())
     print(f"[전송] ID:{motor_id}, DIR:{direction}, SPEED:{speed:.2f}, DIST:{distance:.2f}")
     time.sleep(0.05)
 
-def move(path, r_start, theta_start, linear_speed=0, duration=0, visualize=False):
+def move(path, r_start, theta_start, linear_speed=0, duration=0, visualize=False):  #목적지, 초기값, cm/s, sec, 그림
     if isinstance(path, tuple):
         path = [path]
 
@@ -42,7 +42,7 @@ def move(path, r_start, theta_start, linear_speed=0, duration=0, visualize=False
         dir_r = 'F' if dr >= 0 else 'B'
         dir_theta = 'F' if dtheta >= 0 else 'B'
 
-        send_command(1, dir_r, abs(speed_r), abs(dr))
+        send_command(1, dir_r, abs(speed_r), abs(dr))                #send_command
         send_command(2, dir_theta, abs(speed_theta), abs(dtheta))
 
         if linear_speed > 0 and visualize:
@@ -58,7 +58,7 @@ def move(path, r_start, theta_start, linear_speed=0, duration=0, visualize=False
 
                 t += dt
 
-        time.sleep(move_time + 0.5)
+        time.sleep(move_time + 0.5) #다음 신호를 미리 받는다 하면 0.5-> 1로 바꾸든 증가하면 됨!
         curr_r, curr_theta = r, theta
 
     if linear_speed > 0 and visualize:
