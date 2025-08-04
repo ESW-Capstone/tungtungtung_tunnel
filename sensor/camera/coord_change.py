4a46cda57e8dee9814bee28d27c5eec697d98af1
JonghyukLee-1 <jonghyuk111765@gmail.com>
2025-08-04 16:24:57 +0900

    Update coord_change.py

--- sensor/camera/coord_change.py
+++ sensor/camera/coord_change.py
@@ -1,1 +1,16 @@
-"# ÁÂÇ¥°è º¯È¯ ÇÔ¼ö ±¸Çö ¿¹Á¤" 
+"# ì¢Œí‘œê³„ ë³€í™˜ í•¨ìˆ˜ êµ¬í˜„ ì˜ˆì •" 
+import numpy as np 
+
+
+
+def coord_change(x,y,cx,cy):
+    dx = x - cx
+    dy = y - cy
+    r = (np.sqrt( (dx)**2 + (dy)**2 ))
+    theta = np.arctan2(dx, -dy)
+    theta_deg = (np.degrees(theta))
+    return round(float(r),2),round(float(theta_deg),2)
+
+# print(coord_change(x,y,cx,cy)) >> for
+
+
